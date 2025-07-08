424bb5c67d8a04c48f9a15ce9f3225d6638c2f1b
nadopro <nadopro@gmail.com>
2025-07-08 11:37:24 +0900

    B1137

---
+++ a.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap 5 기본</title>
+  <!-- Bootstrap 5 CSS CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <style>
+
+        .colLine { 
+            line-height:180%; 
+            min-height:30px; 
+            border-bottom:1px dotted #DDDDDD; 
+            padding-top:4px; 
+            padding-bottom:4px;
+        }
+    </style>
+</head>
+<body>
+
+  <div class="container-fluid bg-danger">
+    전체 바
+  </div>
+  <div class="container">
+    <div class="row">
+        <div class="col colLine">
+            PHP를 배워봅시다.<br>
+
+            <?php
+                $age = 13;
+                echo "age = $age<br>";
+
+                for($i=1; $i<=10; $i++)
+                {
+                    echo "$i<br>";
+                }
+            ?>
+
+        </div>
+    </div>
+
+  </div>
+
+  <!-- Bootstrap 5 JS CDN -->
+  
+</body>
+</html>
