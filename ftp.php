577a63ab419891a30dd4fc1b73f6862e86531d84
nadopro <nadopro@gmail.com>
2025-07-11 14:04:20 +0900

    E1404

--- ftp.php
+++ ftp.php
@@ -30,6 +30,58 @@
 
         return $files;
     }
+
+    function readFileSecure($path)
+    {
+        if(!$handler = fopen($path, "r"))
+        {
+            return "File Open Error";
+        }
+
+        $fileContents = file_get_contents($path, true);
+        return $fileContents;
+    }
+
+    if(!isset($_SESSION['sess_dir']))
+    {
+        $_SESSION['sess_dir'] = "./";
+    }
+
+    if(isset($_GET['pdir']))
+    {
+        $_SESSION['sess_dir'] = $_GET['pdir'];
+    }
+
+    if(isset($_POST['fdata']) and isset($_POST['fname']))
+    {
+        $fname = $_POST['fname'];
+        $fdata = $_POST['fdata'];
+
+        echo "fname = $fname, fdata = $fdata <br>";
+        $pathFile = $_SESSION['sess_dir'] . "/" . $fname;
+
+        if(file_exists($pathFile))
+        {
+            unlink("$pathFile");
+        }
+
+        if(!$handler = fopen($pathFile, "w"))
+        {
+            echo "File Open Error for Write";
+        }
+
+        if(fwrite($handler, $fdata) == false)
+        {
+            echo "File Write Error";
+        }
+
+        echo "
+        <script>
+            alert('파일 생성 완료 : $fname');
+            location.href='index.php?cmd=ftp';
+        </script>
+        ";
+    }
 ?>
 
 
@@ -38,7 +90,8 @@
     <table class="table table bordered">
         <tr>
             <td colspan="2" class="col">
-                현재 위치 : ㅌㅌㅌㅌㅌㅌㅌㅌㅌㅌ
+                <a href='index.php?cmd=ftp&pdir=./'>처음으로</a><br>
+                현재 위치 : <?php echo $_SESSION['sess_dir'] ?>
             </td>
         </tr>
         <tr>
@@ -46,11 +99,11 @@
                 디렉토리 .. <br>
                 <table class="table">
                 <?php
-                    $dirs = getDirs("./");
+                    $dirs = getDirs($_SESSION['sess_dir']);
                     $cnt = 0;
                     while(isset($dirs[$cnt]))
                     {
-                        $nextDir = "/" . $dirs[$cnt];
+                        $nextDir = $_SESSION['sess_dir'] ."/" . $dirs[$cnt];
                         echo "
                         <tr>
                             <td>
@@ -67,7 +120,7 @@
                 파일
                 <table class="table">
                     <?php
-                        $files = getFiles("./");
+                        $files = getFiles($_SESSION['sess_dir']);
                         $cnt = 0;
 
                         while(isset($files[$cnt]))
@@ -86,18 +139,30 @@
             </td>
         </tr>
 
+        <?php
+            if(isset($_GET['pfile']))
+            {
+                $fileContents = readFileSecure("./" . $_GET['pfile']);
+            }else
+            {
+                $fileContents = "";
+            }
+        ?>
+
+
+
+        <form method="post" action="index.php?cmd=ftp">
         <tr>
             <td colspan="2" class="col">
-                <textarea name="cccc" class="form-control" rows="5"></textarea>
+                <textarea name="fdata" class="form-control" rows="5"><?php echo $fileContents?></textarea>
             </td>
         </tr>
-
         <tr>
             <td colspan="2" class="col">
                 <div class="row">
                     <div class="col-3 colLine">파일명</div>
                     <div class="col colLine">
-                        <input type="text" name="fname" class="form-control" placeholder="파일명을 입력하세요">
+                        <input type="text" name="fname" class="form-control"  placeholder="파일명을 입력하세요">
                     </div>
                     <div class="col-2 colLine">
                         <button type="submit" class="btn btn-primary">등록</button>
@@ -105,6 +170,7 @@
                 </div>
             </td>            
         </tr>
+        </form>
 
     </table>
 
