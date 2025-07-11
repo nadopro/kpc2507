133ecb1ebc08d44d020ddb676ca2d1f2c60426e9
nadopro <nadopro@gmail.com>
2025-07-11 10:08:26 +0900

    E1008

--- editor.php
+++ editor.php
@@ -12,15 +12,27 @@
     </div>
 </div>
 
+<script>
+    function setCommand(command)
+    {
+        var editor = document.querySelector("#editor");
+        var html = document.querySelector("#html");
+
+        document.execCommand(command);
+        html.innerHTML = editor.innerHTML;
+
+    }
+</script>
+
 <div class="row">
     <div class="col colLine">
-        <button type="button" class="btn btn-gray">
+        <button type="button" class="btn btn-gray" onClick="setCommand('bold')">
             <span class="material-icons">format_bold</span>
         </button>
-        <button type="button" class="btn btn-gray">
+        <button type="button" class="btn btn-gray" onClick="setCommand('underline')">
             <span class="material-icons">format_underline</span>
         </button>
-        <button type="button" class="btn btn-gray">
+        <button type="button" class="btn btn-gray" onClick="setCommand('italic')">
             <span class="material-icons">format_italic</span>
         </button>
     </div>
@@ -31,12 +43,15 @@
 <div class="row">
     <div class="col colLine">
         <div id="editor" contenteditable="true" style="width:100%; height:300px; border:1px solid #888888; padding:5px;" class="rounded" >
-
+동해물과 백두산이 마르고 닳도록 하나님이 보우하사 우리나라 만세
         </div>
     </div>
 </div>
-
-<div class="row">
+<!-- 
+    display:none;
+    display:
+-->
+<div class="row" style="display:">
     <div class="col colLine">
         <textarea id="html" class="form-control" rows="7"></textarea>
     </div>
