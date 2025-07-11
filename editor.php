3e1cd8f32b22b44beab21b44dc9cb7fa5f169009
nadopro <nadopro@gmail.com>
2025-07-11 09:57:19 +0900

    E0957

--- editor.php
+++ editor.php
@@ -2,4 +2,48 @@
     <div class="col colLine">
         <span class="material-icons">settings</span>
     </div>
+    <div class="col colLine text-danger">
+        <span class="material-icons">settings</span>
+    </div>
+    <div class="col colLine text-danger">
+        <button type="button" class="btn btn-primary">
+            <span class="material-icons">settings</span> 설정
+        </button>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col colLine">
+        <button type="button" class="btn btn-gray">
+            <span class="material-icons">format_bold</span>
+        </button>
+        <button type="button" class="btn btn-gray">
+            <span class="material-icons">format_underline</span>
+        </button>
+        <button type="button" class="btn btn-gray">
+            <span class="material-icons">format_italic</span>
+        </button>
+    </div>
+</div>
+<!--
+    WYSIWYG : What You See Is What You Get
+-->
+<div class="row">
+    <div class="col colLine">
+        <div id="editor" contenteditable="true" style="width:100%; height:300px; border:1px solid #888888; padding:5px;" class="rounded" >
+
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col colLine">
+        <textarea id="html" class="form-control" rows="7"></textarea>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col colLine">
+
+    </div>
 </div>
