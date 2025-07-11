7a5b3ed7a739ba0ea0eeb6c79901d87c71904e76
nadopro <nadopro@gmail.com>
2025-07-11 10:49:21 +0900

    E1050

--- config.php
+++ config.php
@@ -2,6 +2,8 @@
     $adminLevel = 9;
     $windowTitle = "KPC 시큐어코딩";
 
+    $smsid = "smsfb";
+    $smskey = "cd30cce0277e1d88154774288a300a05";
     // naming rule
     // window_title
     // my_family_count
