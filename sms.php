9d45f0796647d5820f5ed48c1cbefd9ad12822bf
nadopro <nadopro@gmail.com>
2025-07-11 10:45:51 +0900

    E1045

--- sms.php
+++ sms.php
@@ -1,8 +1,32 @@
-<form method="post">
+<script>
+    function checkError()
+    {
+        var mobile = document.getElementById("mobile");
+        var regex = /^010-[0-9]{4}-[0-9]{4}$/;
+
+        if(!regex.test(mobile.value))
+        {
+            // 실패
+            alert('전화번호 형식은 010-1234-5678 입니다.');
+            mobile.focus();
+            return false;
+        }
+
+        return true;
+    }
+</script>
+
+<form method="post" onSubmit="return checkError()">
 <div class="row">
     <div class="col colLine">
         받는번호 <br>
-        <input type="text" name="mobile" placeholder="010-1234-5678" class="form-control">
+        <input type="text" id="mobile" name="mobile" placeholder="010-1234-5678" class="form-control">
+        <br>
+        <input type="text" name="mobile1" required placeholder="010-1234-5678" class="form-control">
+        <!--
+        <br>
+        <input type="text" name="mobile2" pattern="010-[0-9]{4}-[0-9]{4}" placeholder="010-1234-5678" class="form-control">
+        -->
     </div>
     <div class="col colLine">
         <textarea name="smscontent" rows="5" class="form-control"></textarea>
