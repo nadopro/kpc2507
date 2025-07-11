13467c381c67bc3a941f579d612f0ed97fb0824e
nadopro <nadopro@gmail.com>
2025-07-11 11:12:07 +0900

    E1112

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
     $adminLevel = 9;
     $windowTitle = "KPC 시큐어코딩";
 
-    $smsid = "smskjmin";
-    $smskey = "2b04eb0e81f52b19ed58c5118001fb5";
+    $smsid = "smsfb";
+    $smskey = "cd30cce0277e1d88154774288a300a05";
     $smssender = "010-3540-8848";
     // naming rule
     // window_title
