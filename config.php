7a1fbd977a5067efb8c856f49fa1a96aa85fa7e0
nadopro <70024149+nadopro@users.noreply.github.com>
2025-07-13 13:04:26 +0900

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
     $windowTitle = "KPC 시큐어코딩";
 
     $smsid = "smsfb";
-    $smskey = "cd30cce0277e1d88154774288a300aaa";
-    $smssender = "010-1234-5678";
-    $adminMobile = "010-1234-5678";
+    $smskey = "cd30cce0277e1d88154774288a300aaa"; // 키 값을 배포할 때 변경했습니다.
+    $smssender = "010-1234-5678"; // 보내는 사람의 전화번호, 미리 등록되어 있어야 함.
+    $adminMobile = "010-1234-5678"; // 이상 트래픽 발생시 수신할 관리자의 전화번호
     // naming rule
     // window_title
     // my_family_count
